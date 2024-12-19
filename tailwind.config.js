46a4fd29cf4c6403361143f9807754a92c25c982
Rulzboys <bhalueng28@gmail.com>
2024-12-19 14:03:34 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    fontFamily : {
+      'rubik1' : ["Rubik Doodle Shadow", "system-ui"],
+    },
+    extend: {
+      colors : {
+        balung : '#DEBC33',
+        ijo : '#697C04',
+        anggur : '#704B99',
+        melon : '#BEC47B'
+      },
+     
+    },
+  },
+  plugins: [],
+}
+
